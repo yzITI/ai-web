b5d08839d67d23aae5b9cba283ae112282cd7b25
VampireRabbit <93039237+VampireRabbit@users.noreply.github.com>
2023-11-14 22:00:54 -0500

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+  import SmileIcon from '$lib/components/SmileIcon.svelte'
+  let cot = 0
+
+  function inc () {
+    cot++
+  }
+</script>
+
+<SmileIcon></SmileIcon>
+<p>cot: {cot}</p>
+<button on:click={inc}>Click Me</button>
