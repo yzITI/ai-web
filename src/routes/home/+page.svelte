fd026676f93d4b9c74e9485bcecaf5ec8363c6ce
Phantomlsh <phantomlsh@outlook.com>
2023-11-22 20:29:58 -0600

    Add home functionality

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,8 +1,13 @@
 <script>
+  import { goto } from '$app/navigation'
+  import { balance } from '$lib/store.js'
   import Status from '$lib/components/Status.svelte'
   export let data
+  const acent = () => window.open('https://auth.njsc.ltd/acent.html?token=' + window.sessionStorage.token)
 </script>
 
-<p>home</p>
-<div>{data.user?.name}</div>
+<p>Home (Page in construction)</p>
+<div>User: {data.user?.name}</div>
+<button class="m-4 p-2 bg-gray-200 block" on:click={acent}>Acent Balance: {$balance}</button>
+<button class="m-4 p-2 bg-gray-200 block" on:click={() => { goto('/transcribe') }}>Transcribe</button>
 <Status></Status>
